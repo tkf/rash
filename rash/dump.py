fc35efc3b4b3a7662deb964e7e81d2423d2ed00f
Takafumi Arakaki <aka.tkf@gmail.com>
2013-01-31 21:21:09 +0100

    Add more data points to dump

--- rash/dump.py
+++ rash/dump.py
@@ -22,21 +22,38 @@ def dump_add_arguments(parser):
     parser.add_argument(
         'command',
         help="command that was ran.")
+    parser.add_argument(
+        '--exit-code', type=int,
+        help="exit code $? of the command.")
+    parser.add_argument(
+        '--pipestatus', type=int, nargs='+',
+        help="$pipestatus (zsh) / $PIPESTATUS (bash)")
+    parser.add_argument(
+        '--start', type=int,
+        help='the time COMMAND is started.')
+    parser.add_argument(
+        '--stop', type=int,
+        help='the time COMMAND is finished.')
+    parser.add_argument(
+        '--program',
+        help='like $TERM, but can be anything (e.g., emacs / tmux).')
 
 
-def dump_run(*kwds):
+def dump_run(**kwds):
     """
     Record shell history.
     """
     conf = ConfigStore()
     json_path = os.path.join(conf.dump_path,
                              time.strftime('%Y-%m-%d-%H%M%S.json'))
-    data = kwds
+    data = dict((k, v) for (k, v) in kwds.items() if v is not None)
     data.update(
-        time=int(time.time()),
         shell=os.environ.get('SHELL'),
+        term=os.environ.get('TERM'),
+        path=os.environ.get('PATH'),
         cwd=os.getcwdu(),
     )
+    data.setdefault('stop', int(time.time()))
     with open(json_path, 'w') as fp:
         json.dump(data, fp)
 
