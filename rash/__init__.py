d627a938369f8c3bd420431dedb85970381dca87
Takafumi Arakaki <aka.tkf@gmail.com>
2013-01-31 19:25:27 +0100

    Add rash/cli.py

---
+++ rash/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.0.0.dev0'
+__author__ = 'Takafumi Arakaki'
+__license__ = "MIT License"
---
+++ rash/cli.py
@@ -0,0 +1,57 @@
+"""
+Command line interface.
+"""
+
+import argparse
+import textwrap
+
+
+class Formatter(argparse.RawDescriptionHelpFormatter,
+                argparse.ArgumentDefaultsHelpFormatter):
+    pass
+
+
+def get_parser(commands):
+    """
+    Generate argument parser given a list of subcommand specifications.
+
+    :type commands: list of (str, function, function)
+    :arg  commands:
+        Each element must be a tuple ``(name, adder, runner)``.
+
+        :param   name: subcommand
+        :param  adder: a function takes one object which is an instance
+                       of :class:`argparse.ArgumentParser` and add
+                       arguments to it
+        :param runner: a function takes keyword arguments which must be
+                       specified by the arguments parsed by the parser
+                       defined by `adder`.  Docstring of this function
+                       will be the description of the subcommand.
+
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+    subparsers = parser.add_subparsers()
+
+    for (name, adder, runner) in commands:
+        subp = subparsers.add_parser(
+            name,
+            formatter_class=Formatter,
+            description=runner.__doc__ and textwrap.dedent(runner.__doc__))
+        adder(subp)
+        subp.set_defaults(func=runner)
+
+    return parser
+
+
+def main(args=None):
+    # from . import MODULE
+    parser = get_parser(
+        # + MODULE.commands
+    )
+    ns = parser.parse_args(args=args)
+    applyargs = lambda func, **kwds: func(**kwds)
+    applyargs(**vars(ns))
+
+
+if __name__ == '__main__':
+    main()
