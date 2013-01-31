53275b0168352dc59c86fd71f01c780fdf097d69
Takafumi Arakaki <aka.tkf@gmail.com>
2013-01-31 21:56:27 +0100

    Move code around in rash/dump.py

--- rash/dump.py
+++ rash/dump.py
@@ -18,6 +18,25 @@ import json
 from .config import ConfigStore
 
 
+def dump_run(**kwds):
+    """
+    Record shell history.
+    """
+    conf = ConfigStore()
+    json_path = os.path.join(conf.dump_path,
+                             time.strftime('%Y-%m-%d-%H%M%S.json'))
+    data = dict((k, v) for (k, v) in kwds.items() if v is not None)
+    data.update(
+        shell=os.environ.get('SHELL'),
+        term=os.environ.get('TERM'),
+        path=os.environ.get('PATH'),
+        cwd=os.getcwdu(),
+    )
+    data.setdefault('stop', int(time.time()))
+    with open(json_path, 'w') as fp:
+        json.dump(data, fp)
+
+
 def dump_add_arguments(parser):
     parser.add_argument(
         'command',
@@ -37,25 +56,6 @@ def dump_add_arguments(parser):
     parser.add_argument(
         '--program',
         help='like $TERM, but can be anything (e.g., emacs / tmux).')
-
-
-def dump_run(**kwds):
-    """
-    Record shell history.
-    """
-    conf = ConfigStore()
-    json_path = os.path.join(conf.dump_path,
-                             time.strftime('%Y-%m-%d-%H%M%S.json'))
-    data = dict((k, v) for (k, v) in kwds.items() if v is not None)
-    data.update(
-        shell=os.environ.get('SHELL'),
-        term=os.environ.get('TERM'),
-        path=os.environ.get('PATH'),
-        cwd=os.getcwdu(),
-    )
-    data.setdefault('stop', int(time.time()))
-    with open(json_path, 'w') as fp:
-        json.dump(data, fp)
 
 
 commands = [
