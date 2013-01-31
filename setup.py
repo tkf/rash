7a35ced54929ceb6615a1d1d1951f7b54d15505c
Takafumi Arakaki <aka.tkf@gmail.com>
2013-01-31 19:25:27 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+import rash
+
+setup(
+    name='rash',
+    version=rash.__version__,
+    packages=['rash'],
+    author=rash.__author__,
+    author_email='aka.tkf@gmail.com',
+    url='https://github.com/tkf/rash',
+    license=rash.__license__,
+    description='Rash Advances Shell History',
+    long_description=rash.__doc__,
+    keywords='history, shell',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    install_requires=[
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': ['rash = rash.cli:main'],
+    },
+)
