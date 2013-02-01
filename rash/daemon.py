4db22c2d82e355e5881898e2fcf7a13bbc45bcd7
Takafumi Arakaki <aka.tkf@gmail.com>
2013-02-01 02:27:06 +0100

    Rename: %s/server/daemon/g in rash/daemon.py

--- rash/daemon.py
+++ rash/daemon.py
@@ -1,23 +1,23 @@
-def server_run():
+def daemon_run():
     """
     [UNDER CONSTRUCTION]
-    Run RASH index server.
+    Run RASH index daemon.
 
-    This server watches the directory ``~/.config/rash/data/record``
+    This daemon watches the directory ``~/.config/rash/data/record``
     and translate the JSON files dumped by ``record`` command into
     sqlite3 DB at ``~/.config/rash/data/db.sqlite``.
 
     """
-    # Probably it makes sense to use this server to provide search
-    # API, so that this server is going to be the only process that
+    # Probably it makes sense to use this daemon to provide search
+    # API, so that this daemon is going to be the only process that
     # is connected to the DB?
     raise NotImplementedError
 
 
-def server_add_arguments(parser):
+def daemon_add_arguments(parser):
     pass
 
 
 commands = [
-    ('server', server_add_arguments, server_run),
+    ('daemon', daemon_add_arguments, daemon_run),
 ]
