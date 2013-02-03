tkf/rash

commit bbacea00476ba0fe80a896da0ba2c32016bd9cb5

Add debugging logs to watchrecord.py

rash/watchrecord.py
@@ -36,10 +36,14 @@ def watch_record(indexer):
     event_handler = RecordHandler(indexer)
     observer = Observer()
     observer.schedule(event_handler, path=indexer.record_path, recursive=True)
+    indexer.logging.debug('Start observer.')
     observer.start()
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
+        indexer.logging.debug('Got KeyboardInterrupt. Stopping observer.')
         observer.stop()
+    indexer.logging.debug('Joining observer.')
     observer.join()
+    indexer.logging.debug('Finish watching record.')

commit 6d515beb541630d5c799657880393be5c608da40

Fix watchrecord: use indexer.logger, not logging
This bug was introduced in bbacea0

rash/watchrecord.py
@@ -36,14 +36,14 @@ def watch_record(indexer):
     event_handler = RecordHandler(indexer)
     observer = Observer()
     observer.schedule(event_handler, path=indexer.record_path, recursive=True)
-    indexer.logging.debug('Start observer.')
+    indexer.logger.debug('Start observer.')
     observer.start()
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        indexer.logging.debug('Got KeyboardInterrupt. Stopping observer.')
+        indexer.logger.debug('Got KeyboardInterrupt. Stopping observer.')
         observer.stop()
-    indexer.logging.debug('Joining observer.')
+    indexer.logger.debug('Joining observer.')
     observer.join()
-    indexer.logging.debug('Finish watching record.')
+    indexer.logger.debug('Finish watching record.')