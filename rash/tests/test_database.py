ae2237d67dd0cbe232bfcc12a68067c99134fb57
Takafumi Arakaki <aka.tkf@gmail.com>
2013-02-05 05:55:09 +0100

    Add test_serach_command_by_glob_environ_in_command

--- rash/tests/test_database.py
+++ rash/tests/test_database.py
@@ -421,6 +421,34 @@ class TestInMemoryDataBase(BaseTestCase):
             exclude_environ_pattern=[('SHELL', 'sh')], unique=False)
         self.assertEqual(len(records), 2)
 
+    def test_serach_command_by_glob_environ_in_command(self):
+        data1 = self.get_dummy_command_record_data()
+        data2 = self.get_dummy_command_record_data()
+        # Database does not distinguish record by different environ,
+        # so other key must be different.
+        data1['command'] = 'git status'
+        data2['command'] = 'hg status'
+        data1['environ'] = {'SHELL': 'zsh'}
+        data2['environ'] = {'SHELL': 'bash'}
+        self.db.import_dict(data1)
+        self.db.import_dict(data2)
+        dcrec1 = to_command_record(data1)
+        dcrec2 = to_command_record(data2)
+
+        records = self.search_command_record(
+            include_environ_pattern=[('SHELL', '*sh')], unique=False)
+        self.assert_same_command_record(records[0], dcrec1)
+        self.assert_same_command_record(records[1], dcrec2)
+        self.assertEqual(len(records), 2)
+
+        records = self.search_command_record(
+            include_environ_pattern=[('SHELL', 'sh*')], unique=False)
+        self.assertEqual(len(records), 0)
+
+        records = self.search_command_record(
+            exclude_environ_pattern=[('SHELL', 'sh*')], unique=False)
+        self.assertEqual(len(records), 2)
+
     def search_session_record(self, **kwds):
         return list(self.db.search_session_record(**kwds))
 
