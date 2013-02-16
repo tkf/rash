0f2155aad816e501764e6f5631b12e78bdfc01a5
Takafumi Arakaki <aka.tkf@gmail.com>
2013-02-16 06:13:07 +0100

    Fix py3compat.getcwd
    os.getcwdu never used in the previous version.
    That was wrong.

--- rash/utils/py3compat.py
+++ rash/utils/py3compat.py
@@ -19,9 +19,9 @@ import sys
 PY3 = (sys.version_info[0] >= 3)
 
 try:
-    getcwd = os.getcwd
-except AttributeError:
     getcwd = os.getcwdu
+except AttributeError:
+    getcwd = os.getcwd
 
 
 try:
